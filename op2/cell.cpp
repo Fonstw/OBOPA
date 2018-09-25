14968f188df69ec08759128a985156de411e6477
Unknown <twistfons@live.nl>
2018-09-25 17:55:59 +0200

    Screw strategic coding for now
    Sorry, teacher, I'll ask about it in class ;-)

--- op2/cell.cpp
+++ op2/cell.cpp
@@ -5,3 +5,25 @@
 #include "cell.h"
 
 Cell::~Cell() = default;
+
+void Cell::SetAlive(bool state)
+{
+	alive = state;
+}
+
+bool Cell::GetAlive()
+{
+	return alive;
+}
+
+bool Cell::DeadYet(int neighbours)
+{
+	// return whether cell is dead het
+	return neighbours<min || neighbours>max;
+}
+
+bool Cell::BornAgain(int neighbours)
+{
+	// return whether cell is (re-)born
+	return neighbours == born;
+}
--- op2/main.cpp
+++ op2/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <time.h>
 //#include <vector>
-#include "conwaycell.h"
+#include "cell.h"
 
 // ====== DEFINES ======
 #define GRID_SIZE 25
@@ -16,21 +16,28 @@ using namespace std;
 int currentGeneration = 0;
 
 // ====== CLASSES ======
-ConwayCell grid[GRID_SIZE][GRID_SIZE];
+Cell grid[GRID_SIZE][GRID_SIZE];
 
 // ====== METHODS ======
 int CountNeighbours(int xPos, int yPos)
 {
 	int neighbours = 0;
 
+	// for anything left, center or right from it...
 	for (int x=-1; x<=1; x++)
 	{
+		// ...and anything above, in the middle or under it...
 		for (int y=-1; y<=1; y++)
 		{
 			// Forgive me for the following if-else statement, I know there should be a better way but I can't find it
+
+			// If it's not this cell...
 			if (x == 0 && x == y) {}
+			// ...and it's not outside of the grid (also that's blasphemy)
 			else if (xPos + x >= 0 && xPos + x < GRID_SIZE && yPos + y >= 0 && yPos + y < GRID_SIZE)
 			{
+				// Count that which lives (through the above we basically select all neighbours of which we then count
+				// what's alive)
 				if (grid[xPos + x][yPos + y].GetAlive())
 					neighbours++;
 			}   // end else
@@ -42,19 +49,25 @@ int CountNeighbours(int xPos, int yPos)
 
 void LiveAndDie()
 {
+	// 'Empty' grid to write in, for instantity's sake
 	Cell tempGrid[GRID_SIZE][GRID_SIZE];
 
 	for (int x=0; x<GRID_SIZE; x++)
 	{
 		for (int y=0; y<GRID_SIZE; y++)
 		{
+			// Is anything in the grid dead?
 			if (grid[x][y].DeadYet(CountNeighbours(x, y)))
+				// Say so in the writable grid
 				tempGrid[x][y].SetAlive(false);
+			// bla bla alive?
 			else if (grid[x][y].BornAgain(CountNeighbours(x, y)))
+				// bla bla writable grid
 				tempGrid[x][y].SetAlive(true);
 		}
 	}
 
+	// Copy written grid into actual, cout'ed, current grid
 	memcpy(grid, tempGrid, sizeof(tempGrid));
 }
 
@@ -77,7 +90,7 @@ void PrintGrid()
 
 int main()
 {
-	// Initialise a random field
+	// Initialise a random field through a very bad manner, but that's not too important now
 	srand(time(NULL));
 
 	for (auto &x : grid)
@@ -86,12 +99,20 @@ int main()
 			y.SetAlive(rand()%DENSITY == 0);
 	}
 
+	// The actual Conway's Game of Life'ing starts here!!
 	while (currentGeneration < LAST_GENERATION)
 	{
+		// Tell which generation we're in
 		cout<<"Generation: "<<currentGeneration<<endl;
+
+		// Show this generation's cells
 		PrintGrid();
+		// Kill and birth them
 		LiveAndDie();
-		currentGeneration++;
+		// Separate the generations for readability's sake
 		cout<<endl;
+
+		// Count generation
+		currentGeneration++;
 	}
 }
